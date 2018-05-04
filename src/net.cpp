4be777095e80332e38a73488528cd338800bc9b6
Iluy Sperry <iluysperry@hotmail.com>
2018-05-04 13:47:30 -0300

    Update net.cpp
    Fiz master dns seed.

--- src/net.cpp
+++ src/net.cpp
@@ -1173,6 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+    {"explorer.sperrytecnologia.com.br", "35.200.11.206"},
     {NULL, NULL}
 };
 
